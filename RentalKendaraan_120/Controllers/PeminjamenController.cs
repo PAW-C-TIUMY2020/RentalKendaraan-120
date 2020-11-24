3a82e9bf10e8dd11c6c418aa55bd1a88d311b750
Zulqoniun <muhammadqoni123@gmail.com>
2020-11-24 20:40:10 +0700

    Update Pertemuan

--- RentalKendaraan_120/Controllers/PeminjamenController.cs
+++ RentalKendaraan_120/Controllers/PeminjamenController.cs
@@ -19,7 +19,7 @@ namespace RentalKendaraan_120.Controllers
         }
 
         // GET: Peminjamen
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string ktsd, string searchString, string sortOrder, string currentFilter, int? pageNumber)
         {
             var rental_kendaraanContext = _context.Peminjaman.Include(p => p.IdCustomerNavigation).Include(p => p.IdJaminanNavigation).Include(p => p.IdKendaraanNavigation);
             return View(await rental_kendaraanContext.ToListAsync());
