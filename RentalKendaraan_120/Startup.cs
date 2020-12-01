ee0de2484f98b57fc44cc8226ab027734c3feacf
Zulqoniun <muhammadqoni123@gmail.com>
2020-12-01 21:43:45 +0700

    pertemuan 9

--- RentalKendaraan_120/Startup.cs
+++ RentalKendaraan_120/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace RentalKendaraan_120
 {
@@ -25,6 +27,10 @@ namespace RentalKendaraan_120
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IFileProvider>(
+            new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -34,6 +40,7 @@ namespace RentalKendaraan_120
 
             services.AddDbContext<Models.rental_kendaraanContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
